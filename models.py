4dcf1706babd953f74d87e1a5384f209341bdd5d
siphiwe <siphiwemanda@gmail.com>
2020-05-20 18:47:53 +0100

    sorting models

--- models.py
+++ models.py
@@ -18,7 +18,7 @@ def setup_db(app, database_path=database_path):
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.app = app
     db.init_app(app)
-    db.create_all()
+    # db.create_all()
 
 
 '''
@@ -31,17 +31,17 @@ class Books(db.Model):
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     title = db.Column(db.String)
-    blurb = db.Column(db.String)
-    countries_book_banned = db.Column(db.String)
+    synopsis = db.Column(db.String)
+    banned = db.relationship('Banned', lazy=True)
     author_id = db.Column(db.Integer, db.ForeignKey('authors.id'))
-    bookcover = db.Column(db.String)
+    book_cover = db.Column(db.String)
 
-    def __init__(self, title, blurb, countries_book_banned , author_id, bookcover):
+    def __init__(self, title, synopsis, countries_book_banned, author_id, book_cover):
         self.title = title
-        self.blurb = blurb
+        self.synopsis = synopsis
         self.author = author_id
         self.country_id = countries_book_banned
-        self.bookcover = bookcover
+        self.book_cover = book_cover
 
     def insert(self):
         db.session.add(self)
@@ -58,9 +58,10 @@ class Books(db.Model):
         return {
             'id': self.id,
             'title': self.title,
-            'blurb': self.blurb,
+            'synopsis': self.synopsis,
             'author': self.author_id,
-            'countries': self.countries_book_banned
+            'countries': self.countries_book_banned,
+            'book_cover': self.book_cover
         }
 
 
@@ -94,3 +95,62 @@ class Authors(db.Model):
         return {
             'id': self.id,
             'name': self.author_name}
+
+
+class Country(db.Model):
+    __tablename__ = "country"
+
+    id = db.Column(db.Integer, primary_key=True)
+    country = db.Column(db.String)
+    banned = db.relationship('Banned', lazy=True)
+
+    def __init__(self, country):
+        self.country = country
+
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def format(self):
+        return {
+            'id': self.id,
+            'country': self.country}
+
+
+class Banned(db.Model):
+    __tablename__ = "banned_details"
+
+    id = db.Column(db.Integer, primary_key=True)
+    start_date = db.Column(db.Date)
+    end_date = db.Column(db.Date)
+    book_id = db.Column(db.Integer, db.ForeignKey('books.id'))
+    country_id = db.Column(db.Integer, db.ForeignKey('country.id'))
+
+    def __init__(self, start_date, end_date):
+        self.start_date = start_date
+        self.end_date = end_date
+
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def format(self):
+        return {
+            'id': self.id,
+            'start_date': self.start_date,
+            'end_date': self.end_date
+        }
